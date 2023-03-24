16d361fa9874ca837db09ff6d3a047ec7de08f91
Dmitri Afanasjev <adimas@gmail.com>
2023-03-24 09:21:56 +0200

    Add tauri-plugin-window-state to Cargo.toml, set up window location replacement, and define a panic hook

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::env;
-//use std::fs::File;
-//use std::path::PathBuf;
 use tauri::Manager;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -12,32 +10,32 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use std::panic::set_hook;
+
 fn main() {
-    let url: String = env::args()
+    set_hook(Box::new(|info| {
+        if let Some(s) = info.payload().downcast_ref::<String>() {
+            println!("{}", s);
+        }
+    }));
+
+    let url = env::args()
         .nth(1)
         .expect("Please provide a URL as an argument");
 
     tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .setup(move |app| {
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .setup(move |app: &mut tauri::App| {
+            let window = app.get_window("main").unwrap();
+            //let command = String::from(format!("window.location.replace('{url}')"));
+            //let result = window.eval(command.as_str()).expect("Failed");
 
-        let command = String::from(format!("window.location.replace('{url}')"));
-        let window = app.get_window("main").unwrap();
-        let result = window.eval(command.as_str()).expect("Failed");
-        Ok(result)
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
-
-    /*tauri::Builder::new()
-    .setup(move |app| {
-        let window = app.get_window("main").unwrap();
-        const command: String = String::from(format!("window.location.replace('{url}')"));
-        let result = window.eval(command.as_str()).expect("Failed");
-        Ok(result)
-    })
-    //.build(tauri::generate_context!())
-    .run(tauri::generate_context!())
-    .expect("Error while building tauri application")*/
-
+            let result = window
+                .eval(&format!("window.location.replace('{url}')", url = url))
+                .unwrap();
+            Ok(result)
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error running tauri app")
 }
