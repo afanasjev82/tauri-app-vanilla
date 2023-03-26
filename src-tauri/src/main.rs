a31929360a54f1e0a493d5fe1bfb39589cdf9b7d
Dmitri Afanasjev <adimas@gmail.com>
2023-03-26 07:27:17 +0300

    Add support for command line arguments to specify the URL to open in the Tauri-based application

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,22 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use serde_json::Value;
 use std::env;
 use std::panic::set_hook;
+use tauri::api::cli::Matches;
 use tauri::Manager;
 
-fn setup_window(main_window: &tauri::Window, url: Option<String>) {
+fn setup_window(window: &tauri::Window, url: Option<Value>) {
     match url {
         Some(p) => {
             println!("Window url specified as {p}");
             // Setting window title
-            main_window
-                .set_title(&p)
+            window
+                .set_title(&format!("{p}"))
                 .expect("Could not set window title");
 
             // WebView2 url change
-            main_window
-                .eval(&format!("window.location.replace('{p}')"))
+            window
+                .eval(&format!("window.location.replace({p})"))
                 .expect(&format!("Could not open url: \"{p}\""))
         }
         None => println!("Window url is not specified, using default"),
@@ -26,9 +28,17 @@ fn setup_window(main_window: &tauri::Window, url: Option<String>) {
     #[cfg(debug_assertions)]
     {
         println!("DevTools is enabled");
-        main_window.open_devtools();
-        //main_window.close_devtools();
+        window.open_devtools();
     }
+}
+
+fn extract_cli_matches(matches: Matches, arg_name: &str) -> Option<Value> {
+    return matches
+        .args
+        .get(arg_name)
+        .and_then(|arg_data| Some(arg_data.value.clone()))
+        .map(|u| if u == false { None } else { Some(u) })
+        .flatten();
 }
 
 fn main() {
@@ -38,12 +48,14 @@ fn main() {
         }
     }));
 
-    let url = env::args().nth(1);
-    //.expect("Please provide a URL as an argument");
-
     tauri::Builder::default()
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .setup(move |app: &mut tauri::App| {
+            let url = match app.get_cli_matches() {
+                Ok(matches) => extract_cli_matches(matches, "url"),
+                Err(_) => panic!("Unable to extract args"),
+            };
+            println!("URL: {:?}", url);
             let main_window = app.get_window("main").unwrap();
             setup_window(&main_window, url);
             main_window.show().unwrap();
